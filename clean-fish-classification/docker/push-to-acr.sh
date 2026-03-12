b1f5805ae5a4f3f7251772bd0c6ebcf69611ca19
Brandon Littler <102030887+brandonlittler@users.noreply.github.com>
2026-03-12 22:14:39 +0400

    Update push-to-acr.sh

--- clean-fish-classification/docker/push-to-acr.sh
+++ clean-fish-classification/docker/push-to-acr.sh
@@ -13,18 +13,9 @@ IMAGE_TAG=$(echo "${IMAGE_TAG}" | sed 's/.*\.azurecr\.io\///')
 ACR_LOGIN_SERVER="${REGISTRY_NAME}.azurecr.io"
 FULL_IMAGE_NAME="${ACR_LOGIN_SERVER}/${IMAGE_TAG}"
 
-
-
-echo "Building Docker image..."
 docker build -f docker/Dockerfile -t fish-classifier:latest .
-
-echo "Tagging image for ACR..."
 docker tag fish-classifier:latest ${FULL_IMAGE_NAME}
-
-echo "Logging into Azure Container Registry..."
 az acr login --name ${REGISTRY_NAME}
-
-echo "Pushing image to ACR..."
 docker push ${FULL_IMAGE_NAME}
 
 echo "Success! Image pushed to: ${FULL_IMAGE_NAME}"
